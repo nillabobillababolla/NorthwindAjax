c650c7d2b55dbe57fdcf651c34e81241665ea9ce
Samet Alabey <alabeysamet@gmail.com>
2019-01-23 19:41:41 +0300

    JsonResult Search[HttpGet]

--- NorthwindAjax/Controllers/CategoryController.cs
+++ NorthwindAjax/Controllers/CategoryController.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using NorthwindAjax.Models;
+using NorthwindAjax.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace NorthwindAjax.Controllers
@@ -12,6 +13,48 @@ namespace NorthwindAjax.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult Search(string s)
+        {
+            var key = s.ToLower();
+            if (key.Length < 2 && key != "*")
+            {
+                return Json(new ResponseData()
+                {
+                    message = "2 karakterden daha fazla girmelisiniz.",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+            try
+            {
+                var db = new NorthwindEntity();
+                List<CategoryViewModel> data;
+                data = db.Categories
+                    .OrderBy(x => x.CategoryName)
+                    .ThenBy(y => y.Products.Count)
+                    .Select(z => new CategoryViewModel()
+                    {
+                        CategoryName = z.CategoryName,
+                        Description = z.Description,
+                        ProductCount = z.Products.Count
+                    }).ToList();
+                return Json(new ResponseData()
+                {
+                    message = $"{data.Count} adet kayıt getirildi.",
+                    data = data,
+                    success = true
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseData()
+                {
+                    message = $"Hata: {ex.Message}",
+                    success = false
+                }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
